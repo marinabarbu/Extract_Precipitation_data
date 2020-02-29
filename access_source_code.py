8447ba969e6f13345178eb7d91d0344ea8ac9d26
Marina Barbu <barbumarina.madalina97@gmail.com>
2020-02-29 23:11:37 +0200

    modified the access source code

--- access_source_code.py
+++ access_source_code.py
@@ -1,23 +1,13 @@
-import urllib3
 import requests
-import numpy as np
-import pandas as pd
 import lxml.html as lh
 
-#response = urllib3.urlopen("https://en.tutiempo.net/climate/05-2017/ws-153460.html")
-#page_source = response.read()
-#print(page_source)
 url = "https://en.tutiempo.net/climate/05-2017/ws-153460.html"
 page = requests.get(url)
-
 #print(len(page.text))
 #print(len(np(page.text.split())))
-
 doc = lh.fromstring(page.content)
 table_elements = doc.xpath('//table')
-
 #print([len(T) for T in table_elements[:30]])
-
 tr_elements = table_elements[3].xpath('//tr')
 #print([len(T) for T in tr_elements])
 rows = [r for r in tr_elements if len(r) == 15]
@@ -33,4 +23,6 @@ for r in rows:
 for r in rows:
     col = r[6].text_content()
     print(col)
+
+
 
